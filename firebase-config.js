3a1b9bc4a7a23989648405f08924f112859500c1
SliceDQ <Qwweeerrbi@gmail.com>
2026-07-13 14:04:10 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Конфигурация проекта Firebase "kstyle".
+// Этот файл подключается и к ff.html, и к admin.html — редактировать нужно только его.
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBMVSHvrgZgyIDhzXjojJz7nSIkwfXTJY0",
+  authDomain: "kstyle-49146.firebaseapp.com",
+  projectId: "kstyle-49146",
+  storageBucket: "kstyle-49146.firebasestorage.app",
+  messagingSenderId: "629457082182",
+  appId: "1:629457082182:web:86c2f75432a9986f00b747"
+};
